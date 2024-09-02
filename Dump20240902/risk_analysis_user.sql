de671d98d0797beb5b33b0a58a04b5e6e62191e9
njk5446 <njk5446@naver.com>
2024-09-02 18:02:21 +0900

    sql 업데이트+

--- Dump20240902/risk_analysis_user.sql
+++ Dump20240902/risk_analysis_user.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 8.0.38, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: risk_analysis
 -- ------------------------------------------------------
--- Server version	8.0.39
+-- Server version	8.0.37
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -16,29 +16,37 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `incident_log`
+-- Table structure for table `user`
 --
 
-DROP TABLE IF EXISTS `incident_log`;
+DROP TABLE IF EXISTS `user`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `incident_log` (
-  `idx` int NOT NULL AUTO_INCREMENT,
-  `user_code` int NOT NULL,
-  `incident_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `incident_description` varchar(500) NOT NULL,
-  `resolution` varchar(500) NOT NULL,
-  PRIMARY KEY (`idx`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE `user` (
+  `user_code` int NOT NULL AUTO_INCREMENT,
+  `user_id` varchar(45) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `user_name` varchar(45) NOT NULL,
+  `role` enum('ROLE_ADMIN','ROLE_USER') DEFAULT NULL,
+  `position` varchar(45) DEFAULT NULL,
+  `department` varchar(45) DEFAULT NULL,
+  `region` varchar(45) DEFAULT NULL,
+  `date_of_birth` date DEFAULT NULL,
+  `gender` enum('F','M') DEFAULT NULL,
+  `create_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`user_code`),
+  UNIQUE KEY `user_id_UNIQUE` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `incident_log`
+-- Dumping data for table `user`
 --
 
-LOCK TABLES `incident_log` WRITE;
-/*!40000 ALTER TABLE `incident_log` DISABLE KEYS */;
-/*!40000 ALTER TABLE `incident_log` ENABLE KEYS */;
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'admin','$2a$10$W25g/mahcuAXrrswGbApGeP65HQlkrkvJXiSMCYWzNqhqm1xMXCoe','관리자','ROLE_ADMIN','과장','영업1팀','서울','2024-09-02','M','2024-09-02 05:53:34'),(2,'user111','$2a$10$h.4gTqqB6SaoRgfrELOmV.vo3XgvyECbumm2GwheByhJDn1QFdloC','홍길동','ROLE_USER',NULL,NULL,NULL,NULL,NULL,'2024-09-02 06:40:01');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -50,4 +58,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-09-01 20:49:57
+-- Dump completed on 2024-09-02 18:01:26
